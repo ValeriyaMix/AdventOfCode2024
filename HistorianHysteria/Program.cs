e6a1577930f43b9da31fc78f9c9e307c4ac01a09
ValeriyaMix <lerka-mishka@yandex.ru>
2024-12-01 22:02:45 +0000

    Add solution to the Part 2 of the first task.

--- HistorianHysteria/Program.cs
+++ HistorianHysteria/Program.cs
@@ -12,7 +12,9 @@ namespace MyApp
 
             // Initialize lists to store column values
             List<int> column1 = new List<int>();
+            List<int> column1P2 = new List<int>();
             List<int> column2 = new List<int>();
+            List<int> column2P2 = new List<int>();
 
             foreach (string line in lines)
             {
@@ -22,7 +24,9 @@ namespace MyApp
                 if (columnParts.Length == 2) // Ensure there are exactly two columns
                 {
                     column1.Add(int.Parse(columnParts[0]));
+                    column1P2.Add(int.Parse(columnParts[0]));
                     column2.Add(int.Parse(columnParts[1]));
+                    column2P2.Add(int.Parse(columnParts[1]));
                 }
             }
 
@@ -39,6 +43,18 @@ namespace MyApp
             }
             int answer = listDifferences.Sum();
             Console.WriteLine(answer);
+
+            //Part two
+            List<int> listSimularityScore = new List<int>();
+            foreach (int value in column1P2)
+            {
+                List<int> foundMatches = column2P2.FindAll(x => x == value);
+                int countMatches = foundMatches.Count;
+                listSimularityScore.Add(value * countMatches);
+            }
+
+            int answerPartTwo = listSimularityScore.Sum();
+            Console.WriteLine(answerPartTwo);
         }
 
         public static string[] readFromTheFile(string folderName)
